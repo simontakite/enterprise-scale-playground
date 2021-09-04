521395db23dbe7c929b0843009332b62786048b9
Simon Takite <simon@devopsconsults.ug>
2021-09-04 12:33:06 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# Use variables to customise the deployment
+
+variable "root_id" {
+  type    = string
+  default = "devopscon"
+}
+
+variable "root_name" {
+  type    = string
+  default = "DevOps Consults Org"
+}
+
+variable "default_location" {
+  type    = string
+  default = "norwayeast"
+}
